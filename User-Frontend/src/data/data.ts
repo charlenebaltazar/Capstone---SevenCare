f00793f7e7740e52b68a4fc96cd46e774b8ea2c8
charlenebaltazar <chrlnbltzr5@gmail.com>
2025-09-04 13:23:47 +0800

    update data.ts

--- User-Frontend/src/data/data.ts
+++ User-Frontend/src/data/data.ts
@@ -1,4 +1,4 @@
-export const BACKEND_DOMAIN = "https://sevencare-backend.onrender.com";
+export const BACKEND_DOMAIN = "https://sevencare-backend-l7g6.onrender.com";
 
 export const whyChooseUs = [
   "Transparent Pricing",
