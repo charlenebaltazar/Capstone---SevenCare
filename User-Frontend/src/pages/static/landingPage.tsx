64d6db7c6299034f1e560b57d13495fc060fa21c
DizonNestor <nestor.dizonn@gmail.com>
2025-09-12 17:18:58 +0800

    added module 2

--- User-Frontend/src/pages/static/landingPage.tsx
+++ User-Frontend/src/pages/static/landingPage.tsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
 import Footer2 from "../../components/Footer2";
 import Header from "../../components/Header";
 
 export default function LandingPage() {
   return (
-    <main className="flex flex-col w-full h-screen font-roboto bg-zinc-100">
+    <main className="flex flex-col w-full h-screen font-roboto bg-bg-color">
       <Header active="home" />
       <section className="flex-1 flex w-full flex-row justify-center items-center gap-5">
         <aside className="flex-col flex gap-2 justify-center items-center w-52">
           <img src="/assets/icons/statoscope.png" />
-          <button className="w-full py-2 bg-primary rounded-lg text-zinc-50">
+          <Link
+            to="/login"
+            className="w-full py-2 bg-primary rounded-lg text-zinc-50 flex  justify-center items-center font-bold"
+          >
             Make an Appointment
-          </button>
+          </Link>
         </aside>
 
         <h1 className="text-5xl leading-16">
