88338a34ce94cb847d387022e565998ee7e13f96
DizonNestor <nestor.dizonn@gmail.com>
2025-09-12 16:32:31 +0800

    celestra added module 2

---
+++ User-Frontend/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-full w-full justify-center items-center flex">
+      <p className="p-5 text-2xl font-bold">Loading...</p>
+    </div>
+  );
+}
